908270dbe839e2a3a821bb44e78f7d166307f1ac
Bang Nguyen Ngoc <n2bang89@gmail.com>
2020-02-14 17:08:08 +0700

    init vue demo

---
+++ src/components/AppComponent.vue
@@ -0,0 +1,12 @@
+// AppComponent.vue
+<style type="text/css">
+	h1 { margin: 0; }
+</style>
+<template>
+    <h1>Hello from the AppComponent</h1>
+</template>
+<script>
+    export default {
+
+    }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+// webpack.config.js
+
+const path = require("path");
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports =  {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            data: '$color: red'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
